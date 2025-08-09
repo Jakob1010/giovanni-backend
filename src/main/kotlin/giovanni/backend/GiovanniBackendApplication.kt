5b10aa239b732ee7555f460e0024ed1d024af8ab
Jakob Aichinger <aichinger.j.k@gmail.com>
2025-08-09 11:43:01 +0200

    init spring app

---
+++ src/main/kotlin/giovanni/backend/GiovanniBackendApplication.kt
@@ -0,0 +1,11 @@
+package giovanni.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GiovanniBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<GiovanniBackendApplication>(*args)
+}
---
+++ src/test/kotlin/giovanni/backend/GiovanniBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package giovanni.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GiovanniBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
