c6eaaa44b0e0c16f476dd10cc2f6f4a6b01c7ed9
Jakob Aichinger <aichinger.j.k@gmail.com>
2025-08-15 23:43:17 +0200

    cors

--- src/main/kotlin/giovanni/backend/controller/CustomerController.kt
+++ src/main/kotlin/giovanni/backend/controller/CustomerController.kt
@@ -4,6 +4,7 @@ import giovanni.backend.dto.CustomerResponse
 import giovanni.backend.dto.EyeConfigResponse
 import giovanni.backend.dto.GlassesConfigResponse
 import giovanni.backend.entity.EyeSide
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -14,11 +15,11 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.time.Instant
 import java.time.LocalDate
-import java.time.LocalDateTime
 import java.util.UUID
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/api/v1/customers")
+@CrossOrigin(origins = ["http://localhost:4200"])
 class CustomerController {
 
     @PostMapping
